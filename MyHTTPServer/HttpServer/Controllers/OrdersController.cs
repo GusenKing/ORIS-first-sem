88dd5990541e061df6fa892571d99b395c171ccc
Emin <emin.sarux2004@yandex.ru>
2023-10-16 13:14:47 +0300

    steam parser working and returning page with cases with a button

--- MyHTTPServer/HttpServer/Controllers/OrdersController.cs
+++ MyHTTPServer/HttpServer/Controllers/OrdersController.cs
@@ -1,9 +1,6 @@
 ﻿using System.Net;
-using System.Net.Mime;
 using HtmlAgilityPack;
 using HttpServer.Attribuets;
-using MimeKit;
-
 namespace HttpServer.Controllers;
 
 [Controller("Orders")]
@@ -18,14 +15,6 @@ public class OrdersController
         SteamMarketParser.GetCaseList(html);
         html.LoadHtml(wClient.DownloadString("https://steamcommunity.com/market/search?q=#p2_popular_desc"));
         SteamMarketParser.GetCaseList(html);
-
-        // string page;
-        // using (StreamReader sr = new StreamReader("hewPage.html"))
-        // {
-        //     page = sr.ReadToEndAsync().Result;
-        // }
-        // return page;
         return SteamMarketParser.GetPage();
     }
-    
 }
---
+++ MyHTTPServer/HttpServer/Services/Parsers/SteamMarketParser.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using HtmlAgilityPack;
+
+namespace HttpServer;
+
+public static class SteamMarketParser
+{
+    private static List<HtmlNode> _caseListingBlocks = new List<HtmlNode>();
+    
+    public static void GetCaseList(HtmlDocument html)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var buttonNode = HtmlNode.CreateNode("<button>Купить</button>");
+            var parsedNode = html.GetElementbyId($"result_{i}");
+            parsedNode.AppendChild(buttonNode);
+            _caseListingBlocks.Add(parsedNode);
+        }
+    }
+
+    public static string GetPage()
+    {
+        var newHtmlDoc = new HtmlDocument();
+        var headHtml = @"<head>
+    <meta charset=""UTF-8"">
+    <style>
+        .market_listing_row{
+            border: black solid 1px;
+            padding-bottom: 15%;
+            margin-right: 20%;
+            margin-bottom: 20%;
+        }
+    </style>
+</head>";
+        var headNode = HtmlNode.CreateNode(headHtml);
+        newHtmlDoc.DocumentNode.AppendChild(headNode);
+        var wrapperHtml = @"<div class=""wrapper"" style=""display: grid; grid-template-columns: 1fr 1fr 1fr""></div>";
+        var wrapperNode = HtmlNode.CreateNode(wrapperHtml);
+        newHtmlDoc.DocumentNode.AppendChild(wrapperNode);
+        
+        for (int i = 0; i < 15; i++)
+        {
+                wrapperNode.AppendChild(_caseListingBlocks[i]);
+        }
+
+        var res = newHtmlDoc.DocumentNode.WriteTo();
+        return res;
+    }
+}
